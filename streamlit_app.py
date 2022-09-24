4633cf10eb8babc88037f4120e915b1357869afb
SnowflakeLabber <114251292+SnowflakeLabber@users.noreply.github.com>
2022-09-24 07:11:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ except URLError as e:
 
 streamlit.header("Snowflake Power!")
 
-def get_fruit_load_list();
+def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
       my_cur.execute("SELECT * FROM fruit_load_list")
       return my_cur.fetchall()
